0fa6234a8124bff1a66d17dea2dbe663f6f9b6c9
pneu <pneumaster@gmail.com>
2013-09-23 19:53:09 +0900

    Why writeFile doesn't work?

--- endec.hs
+++ endec.hs
@@ -1,30 +1,70 @@
+{-# LANGUAGE FlexibleInstances #-}
 module ENDEC where
 
-import Prelude hiding (readFile)
+import Prelude hiding (readFile, writeFile)
 import Data.Char (isAlphaNum)
-import Data.ByteString.Char8 (ByteString, pack, readFile)
+import Data.ByteString.Char8 (ByteString, pack, unpack, readFile, writeFile, hPut)
 import Data.Functor ((<$>))
+import qualified Data.ByteString.Base64 as B64 (encode, decode)
+import Control.Applicative (pure, (<*>))
+import Control.DeepSeq (($!!), NFData(..))
 import Data.SecureMem (toSecureMem)
 import Crypto.Cipher
 import Crypto.Cipher.Types
+import System.IO (withBinaryFile, IOMode(WriteMode), hSetBinaryMode)
 
---main = undefined
+instance NFData KeyError
+instance NFData (IO ())
+
 main = do
-  key <- initialize
-  encode key
-  return ()
+  b <- doReadfile
+  print b
+  k <- initialize b
+  let c = encode <$> k
+  print c
+  let a = B64.encode <$> c
+  print a
+  --let d = writefile <$> a
+  let d = id $!! (writefile <$> a)
+  --let x = id $!! d
+  --let e = decode <$> k <*> c
+  let g = B64.decode <$> a
+  print g
+  let e = decode' k g
+  print e
+  return d
 
-initialize :: IO (Either KeyError DES)
-initialize = do
-  --s <- readfile
-  --s <- readline
-  s <- readfixstr
-  let t = (takekey . toDESKey) s
+doReadfile :: IO ByteString
+doReadfile = readfixstr
+
+initialize :: ByteString -> IO (Either KeyError DES)
+initialize s = do
+  let t = takekey <$> toDESKey s
   let x = ciphername t in print x
   let x = cipherkeysize t in print x
   return t
 
-encode = undefined
+encode :: DES -> ByteString
+encode = flip ecbEncrypt (pack "ciphered")
+
+decode :: DES -> ByteString -> ByteString
+decode = ecbDecrypt
+
+decode' :: Either KeyError DES
+            -> Either KeyError (Either String ByteString)
+            -> Either KeyError (Either String ByteString)
+decode' k g = case k of
+                Left ke -> error "error key"
+                Right k' -> case g of
+                    Left e -> error "error"
+                    Right a -> case a of
+                        Left e' -> error "error2"
+                        Right a' -> return $ return $ ecbDecrypt k' a'
+
+writefile :: ByteString -> IO ()
+writefile = writeFile ".\\ciphered.txt"
+
+eval a = a `seq` ()
 
 -- buggy
 readfile :: IO ByteString
@@ -38,12 +78,14 @@ readfixstr :: IO ByteString
 readfixstr = (return . pack . filter isAlphaNum) "password"
 
 toDESKey :: ByteString -> Either KeyError (Key DES)
-toDESKey = makeKey . toSecureMem
+--toDESKey = makeKey . toSecureMem
+toDESKey = makeKey
 
-takekey a = cipherInit <$> a
+takekey :: Key DES -> DES
+takekey = cipherInit
 
 --- * helper for debug print
-ciphername a = cipherName <$> a
-cipherkeysize a = cipherKeySize <$> a
+ciphername = (cipherName <$>)
+cipherkeysize = (cipherKeySize <$>)
 
 -- vim: set ts=2 sts=2 sw=2 ai et tw=78:
